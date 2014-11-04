bb9a74af647227447467300785f819512f6a57c4
Adrian Perez de Castro <aperez@igalia.com>
2014-11-04 22:15:08 +0200

    Implement PAM user store using simplepam
    Implements omni.stores.pam.PAMStore using the simplepam module. The PAM
    service name can be optionally configured to be other than "login".

---
+++ omni/stores/pam.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2014 Adrian Perez <aperez@igalia.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Authenticate users using PAM.
+
+Uses PAM (via the `simplepam <https://pypi.python.org/pypi/simplepam/>`_
+module) to authenticate users using a PAM service.
+
+Configuration options
+=====================
+
+``service`` (optional)
+    Name of the PAM service used to perform authentication. If not provided
+    the ``login`` service is used by default.
+"""
+
+from .. import store
+
+
+class PAMStore(store.Base):
+    def __init__(self, service = None):
+        super(PAMStore, self).__init__()
+        self._service = service
+
+    def authenticate(self, username, password):
+        import simplepam
+        return simplepam.authenticate(username, password, self._service)
+
+
+def from_config(config):
+    return PAMStore(config.get("service", None))
