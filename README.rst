01fbe31e892261af693765f4425822ba4137d921
Adrian Perez de Castro <aperez@igalia.com>
2014-11-04 17:47:49 +0200

    Initial import

---
+++ README.rst
@@ -0,0 +1,34 @@
+====
+OMNI
+====
+
+Installation
+============
+
+The stable releases are uploaded to `PyPI <https://pypi.python.org>`_, so
+you can install and upgrade using ``pip``::
+
+  pip install omni
+
+Alternatively, you can install development versions —at your own risk—
+directly from the Git repository::
+
+  pip install -e git://github.com/aperezdc/omni
+
+Configuration
+=============
+
+.. TODO
+
+Development
+===========
+
+If you want to contribute to OMNI, please use the usual GitHub workflow:
+
+1. Clone the repository.
+2. Hack on your clone of OMNI.
+3. Send a pull request for review.
+
+If you do not have programming skills, you can still contribute by
+`reporting issues <https://github.com/aperezdc/omni/issues>`_ that you may
+encounter.
---
+++ setup.py
@@ -0,0 +1,68 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2014 Adrian Perez <aperez@igalia.com>
+#
+# Distributed under terms of the MIT license.
+
+from setuptools import setup
+from setuptools import find_packages
+from os import path
+from email import parser
+from email import message
+from re import match
+
+
+class Metadata(message.Message, object):
+    @staticmethod
+    def get_author_field(name, field):
+        return match(r"^([^<]+)\s+<([^>]*)>$", name).group(field)
+    def get_multiline(self, name):
+        return [line.strip() for line in self[name].splitlines() if line]
+
+    description = property(lambda self: self["Description"])
+    version = property(lambda self: self["Version"])
+    package = property(lambda self: self["Package"])
+    url = property(lambda self: self["URL"])
+    license = property(lambda self: self["License"])
+    main_author_name = property(lambda self:
+            self.get_author_field(self.authors[0], 1))
+    main_author_email = property(lambda self:
+            self.get_author_field(self.authors[0], 2))
+    test_requirements = property(lambda self:
+            self.get_multiline("Test-Requirements"))
+    requirements = property(lambda self: self.get_multiline("Requirements"))
+    classifiers = property(lambda self: self.get_multiline("Classifiers"))
+    authors = property(lambda self: self.get_multiline("Authors"))
+
+
+
+def metadata():
+    with open(path.join(path.dirname(__file__), "omnilib", "=META"), "rU",
+            encoding="utf-8") as f:
+        return parser.Parser(Metadata).parse(f)
+metadata = metadata()
+
+
+def file_contents(*relpath):
+    with open(path.join(path.dirname(__file__), *relpath), "rU",
+            encoding="utf-8") as f:
+        return f.read()
+
+
+setup(
+    name=metadata.package,
+    version=metadata.version,
+    description=metadata.description,
+    long_description=file_contents("README.rst"),
+    author=metadata.main_author_name,
+    author_email=metadata.main_author_email,
+    url=metadata.url,
+    packages=find_packages(),
+    tests_require=metadata.test_requirements,
+    install_requires=metadata.requirements,
+    license=metadata.license,
+    classifiers=metadata.classifiers,
+    include_package_data=True,
+)
