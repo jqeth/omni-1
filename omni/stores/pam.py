735239b1d25b2ba596f20d0afb7df671409964ab
Adrian Perez de Castro <aperez@igalia.com>
2014-11-10 20:56:29 +0200

    Implement listing users for PAMStores
    Add an implementation of Authenticator.usernames() to
    omni.stores.pam.PAMStore.

--- omni/stores/pam.py
+++ omni/stores/pam.py
@@ -18,20 +18,34 @@ Configuration options
 ``service`` (optional)
     Name of the PAM service used to perform authentication. If not provided
     the ``login`` service is used by default.
+
+``min-uid`` (optional)
+    Users with an UID smaller that this value will not be useable. This is
+    typically used to hide special system users from user listings. The
+    default value is `1000`, which is a typical value for GNU/Linux systems
+    and some *BSD systems as well.
 """
 
 from .. import store
 
 
 class PAMStore(store.Base):
-    def __init__(self, service = None):
+    def __init__(self, service=None, min_uid=0):
         super(PAMStore, self).__init__()
         self._service = service
+        self._min_uid = min_uid
 
     def authenticate(self, username, password):
+        # XXX: Should we be picky and check for validity of the UID?
         import simplepam
         return simplepam.authenticate(username, password, self._service)
 
+    def usernames(self):
+        # FIXME: This works but it might be better to use NSS directly.
+        import pwd
+        return (pw.pw_name for pw in pwd.getpwall()
+                if pw.pw_uid >= self._min_uid)
+
 
 def from_config(config):
-    return PAMStore(config.get("service", None))
+    return PAMStore(config.get("service", None), config.get("min-uid", 1000))
