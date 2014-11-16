df0869ab578b2739a87a66ca56cdffffb71f85e6
Adrian Perez de Castro <aperez@igalia.com>
2014-11-16 12:41:31 +0200

    Allow Path to validate both byte and unicode strings
    Make omni.valid.Path validate byte strings as valid paths. The system
    functions that accept paths work with both, so it makes sense to do so.

--- omni/valid.py
+++ omni/valid.py
@@ -10,7 +10,7 @@
 Utilities for dealing with configuration files.
 """
 
-from six import text_type
+from six import text_type, string_types
 from schema import Schema, SchemaError
 from schema import Optional, And, Or, Use
 from functools import wraps
@@ -51,7 +51,7 @@ Password       = text_type
 Text           = And(text_type, len)
 Identifier     = And(text_type, Match(r"^\w+$"), error="Invalid identifier")
 DotIdentifier  = And(text_type, Match(r"^[\w\.]+$"), error="Invalid identifier")
-Path           = And(text_type, Use(path.realpath), error="Invalid path")
+Path           = And(Or(text_type, *string_types), Use(path.realpath), error="Invalid path")
 NaturalNumber  = And(int, lambda v: v >= 0, error="Not a positive number")
 PortNumber     = And(int, lambda v: 0 < v <= 65535)
 Hostname       = Match(r"^[\w][\.\w]*$")
